c55cb2a5ddac68afc36b2ce807f9b0a1f1b10b79
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 15:45:01 -0700

    adds spec for NasaService#neo_feed returning hash of near_earth_objects

--- spec/services/nasa_service_spec.rb
+++ spec/services/nasa_service_spec.rb
@@ -6,7 +6,16 @@ describe NasaService, type: :service do
       it 'returns list of asteroids given date range' do
         service = NasaService.new(ENV["NASA_KEY"])
         asteroids = service.neo_feed("2018-01-01", "2018-01-07")
-        binding.pry
+        
+        expect(asteroids).to have_key(:"2018-01-04")
+        asteroids = asteroids[:"2018-01-04"]
+        expect(asteroids).to be_a(Array)
+        asteroid = asteroids.first
+
+        expect(asteroid).to have_key(:links)
+        expect(asteroid).to have_key(:name)
+        expect(asteroid).to have_key(:nasa_jpl_url)
+        expect(asteroid).to have_key(:neo_reference_id)
       end
     end
   end
