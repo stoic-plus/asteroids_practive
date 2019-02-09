98c6a047ded968829ffb1dadca13eec9252a6064
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 16:41:17 -0700

    adds retrieving date w/ asteroids to controller

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,19 +1,11 @@
 class MostDangerousDayController < ApplicationController
   def index
-    @start_date = format_time(params[:start_date])
-    @end_date = format_time(params[:end_date])
+    @start_date = format_MONTH_D_YYY(params[:start_date])
+    @end_date = format_MONTH_D_YYY(params[:end_date])
 
-    @nasa_facade = NasaFacade.new(date_params)
-  end
-
-  private
-
-  def format_time(time_string)
-    year, month, day = time_string.split("-")
-    Time.new(year, month, day).strftime('%B%e, %Y')
-  end
-
-  def date_params
-    params.permit(:start_date, :end_date)
+    @nasa_facade = NasaFacade.new(ENV["NASA_KEY"])
+    date_w_asteroids = @nasa_facade.most_dangerous_day(params[:start_date], params[:end_date])
+    @most_dangerous_date = format_MONTH_D_YYY(date_w_asteroids[0].to_s)
+    @dangerous_asteroids = date_w_asteroids[1]
   end
 end
