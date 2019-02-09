f94db9371dc102c72de68f6e476ccc7bba5a5b12
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 14:52:40 -0700

    adds spec for #most_dangerous_day

--- spec/facades/nasa_facade_spec.rb
+++ spec/facades/nasa_facade_spec.rb
@@ -5,4 +5,13 @@ describe NasaFacade, type: :facade do
     nasa_facade = NasaFacade.new("2018-01-01", "2018-01-07")
     expect(nasa_facade).to be_a(NasaFacade)
   end
+
+  describe 'instance methods' do
+    context '#most_dangerous_day' do
+      it 'returns day with the most asteroids with \'is_potentially_dangerous_asteroid\': true' do
+        nasa_facade = NasaFacade.new("2018-01-01", "2018-01-07")
+        expect(nasa_facade.most_dangerous_day).to eq("January 1, 2018")
+      end
+    end
+  end
 end
