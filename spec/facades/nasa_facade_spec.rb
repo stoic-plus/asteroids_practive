1686543fe50ca1818ef9596bfacce822b78636a5
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 15:51:25 -0700

    updates most_dangerous_day spec

--- spec/facades/nasa_facade_spec.rb
+++ spec/facades/nasa_facade_spec.rb
@@ -9,8 +9,8 @@ describe NasaFacade, type: :facade do
   describe 'instance methods' do
     context '#most_dangerous_day' do
       it 'returns day with the most asteroids with \'is_potentially_dangerous_asteroid\': true' do
-        nasa_facade = NasaFacade.new("2018-01-01", "2018-01-07")
-        expect(nasa_facade.most_dangerous_day).to eq("January 1, 2018")
+        nasa_facade = NasaFacade.new(ENV["NASA_KEY"])
+        expect(nasa_facade.most_dangerous_day("2018-01-01", "2018-01-07")).to eq("January 1, 2018")
       end
     end
   end
