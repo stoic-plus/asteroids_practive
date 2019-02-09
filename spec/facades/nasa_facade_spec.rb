19a336e4fbc3e0c4a6de78fe4ef176832d7b05b5
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 15:15:56 -0700

    nasaface instatiated with api key only

--- spec/facades/nasa_facade_spec.rb
+++ spec/facades/nasa_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe NasaFacade, type: :facade do
   it 'exists' do
-    nasa_facade = NasaFacade.new("2018-01-01", "2018-01-07")
+    nasa_facade = NasaFacade.new(ENV["NASA_KEY"])
     expect(nasa_facade).to be_a(NasaFacade)
   end
 
