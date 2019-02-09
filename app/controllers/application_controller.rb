577e95501663131f69a172518990c0cf7ad51421
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 16:40:11 -0700

    adds format_time helper to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def format_MONTH_D_YYY(time_string)
+    year, month, day = time_string.split("-")
+    Time.new(year, month, day).strftime('%B%e, %Y')
+  end
 end
