55ea869dbac94caee007545f580962a7e9dca936
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 14:42:19 -0700

    adds using nasa facade in most_dange_day controll

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -2,6 +2,8 @@ class MostDangerousDayController < ApplicationController
   def index
     @start_date = format_time(params[:start_date])
     @end_date = format_time(params[:end_date])
+
+    @nasa_facade = NasaFacade.new(date_params)
   end
 
   private
@@ -9,5 +11,9 @@ class MostDangerousDayController < ApplicationController
   def format_time(time_string)
     year, month, day = time_string.split("-")
     Time.new(year, month, day).strftime('%B%e, %Y')
+  end
+
+  def date_params
+    params.permit(:start_date, :end_date)
   end
 end
