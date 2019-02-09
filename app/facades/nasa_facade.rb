9298899030c6d89ad9cca64f8971926d695919ac
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-09 15:51:48 -0700

    adds helper method to create asteroid objects

--- app/facades/nasa_facade.rb
+++ app/facades/nasa_facade.rb
@@ -1,15 +1,23 @@
 class NasaFacade
-  def initialize(nasa_key, start_date, end_date)
+  def initialize(nasa_key)
     @nasa_key = nasa_key
   end
 
-  def most_dangerous_day
-
+  def most_dangerous_day(start_date, end_date)
+    asteroids = asteroids_for_range(start_date, end_date)
+    binding.pry
   end
 
   private
 
   def service
-    NasaService.new
+    NasaService.new(@nasa_key)
+  end
+
+  def asteroids_for_range(start_date, end_date)
+    asteroids = service.neo_feed(start_date, end_date)
+    asteroids.map do |date, near_earth_objects|
+      Asteroid.new(date, near_earth_objects)
+    end
   end
 end
