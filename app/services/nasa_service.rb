7098892803583f960c85a733ee755f03e056e6db
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-10 16:29:17 -0700

    adds helper method - makes get req to conn with params if given

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -4,20 +4,24 @@ class NasaService
   end
 
   def neo_feed(start_date, end_date)
-    response = conn.get do |req|
-      req.url "neo/rest/v1/feed?"
-      req.params["api_key"] = @api_key
-      req.params['start_date'] = start_date
-      req.params['end_date'] = end_date
-    end
-    JSON.parse(response.body, symbolize_names: true)[:near_earth_objects]
+    get_json("neo/rest/v1/feed?", start_date, end_date)[:near_earth_objects]
+  end
+
+  def neo_lookup(neo_reference_id)
+    get_json("neo/rest/v1/neo/#{neo_reference_id}")
   end
 
   private
-  #
-  # def get_json(path)
-  #
-  # end
+
+  def get_json(path, start_date=nil, end_date=nil)
+    response = conn.get do |req|
+      req.url path
+      req.params["api_key"] = @api_key
+      req.params['start_date'] = start_date if start_date
+      req.params['end_date'] = end_date if end_date
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
   def conn
     Faraday.new(url: "https://api.nasa.gov") do |faraday|
